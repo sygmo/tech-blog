a6efb1661af72e3bde4a8a86a7d177a0315d0d29
Sydney Mercier <sydney.mercier91@gmail.com>
2022-03-08 19:41:22 -0600

    display comments on posts

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -1,5 +1,5 @@
 const router = require('express').Router();
-const { Post, User } = require('../models');
+const { Post, Comment, User } = require('../models');
 const withAuth = require('../utils/auth');
 
 router.get('/', async (req, res) => {
